b6eeb82666396c74dfe8789127c6a824cff31af5
Brendon Reyell <brendon@reyell.net>
2025-07-22 22:13:42 -0500

    lots of homepage stuff

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ import MuxPlayer from "@mux/mux-player";
 import autoAnimate from '@formkit/auto-animate'
 
 // import gaLink from './resources/js/alpine/ga-link.js'
-import countUp from './resources/js/alpine/count-up.js'
+// import countUp from './resources/js/alpine/count-up.js'
 
 Alpine.plugin([focus])
 
 window.Alpine = Alpine
 
-Alpine.data('countUp', countUp)
+// Alpine.data('countUp', countUp)
 
 Alpine.directive('auto-animate', (el, { expression }, { evaluate }) => {
 	autoAnimate(el, (expression && evaluate(expression)) || {})
