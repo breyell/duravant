706a4e142804a108566d2aa9d87eec26c1101a59
Brendon Reyell <brendon@reyell.net>
2025-08-20 21:54:46 +0530

    more components

--- functions/tinymce.php
+++ functions/tinymce.php
@@ -3,13 +3,36 @@
 if (!function_exists('timber_vite_tiny_mce_before_init')) {
 	function timber_vite_tiny_mce_before_init($mce_init)
 	{
-		error_log('TinyMCE Plugins: ' . print_r($mce_init, true));
-		$mce_init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;';
+		$mce_init['formats'] = json_encode([
+			'lead' => [
+				'selector' => 'p',
+				'block'    => 'p',
+				'classes'  => 'lead',
+			],
+		], JSON_THROW_ON_ERROR);
+
+		$block_formats = [
+			'Paragraph=p',
+			'Lead Paragraph=lead',
+			'Heading 2=h2',
+			'Heading 3=h3',
+			'Heading 4=h4',
+			'Heading 5=h5',
+			'Heading 6=h6',
+			'Preformatted=pre',
+		];
+		$mce_init['block_formats'] = implode(';', $block_formats);
 
 		return $mce_init;
 	}
 }
 add_filter('tiny_mce_before_init', 'timber_vite_tiny_mce_before_init');
+
+function my_theme_add_editor_styles()
+{
+	add_editor_style('wp-editor-style.css');
+}
+add_action('init', 'my_theme_add_editor_styles');
 
 // Add table controls to the TinyMCE toolbar
 function add_tinymce_table_button($buttons)
